13f69b2003ae4cbe66f11fbe48db0037267ea3cc
Manish kumar <manish@evakoss.com>
2018-01-17 20:45:36 +0530

    changed file

--- post-slack-message.php
+++ post-slack-message.php
@@ -1,6 +1,6 @@
 <?php
 
- // slack custom message code added New
+    // slack custom message code added
 
     // Create a constant to store your Slack URL
     define('SLACK_WEBHOOK', 'https://hooks.slack.com/services/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');
